43be4c09432e3082ff3e88e056adf4d998e5eb2e
Andrew Photinakis <andrewcphotinakis@gmail.com>
2023-08-15 10:30:50 -0400

    initial commit

---
+++ src/main/java/com/finvizapi/webfinvizapi/WebfinvizapiApplication.java
@@ -0,0 +1,13 @@
+package com.finvizapi.webfinvizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfinvizapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfinvizapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finvizapi/webfinvizapi/WebfinvizapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finvizapi.webfinvizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfinvizapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
